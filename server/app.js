ed5ff111882056ddfebfeadd2c473ebcb7660db3
Arifuddin Siddiqui <arifsiddiqui2905@gmail.com>
2025-08-12 11:00:30 +0530

    fixed port issue

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use(cookieParser());  
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/users", userRoutes);
@@ -29,7 +29,7 @@ app.use("/api/images", imageRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 // error handler
 // error handler for API
